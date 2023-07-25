3d8c8b291c4bfa9476127d1d74392547f842e7ea
Harshath kumar Bayyaram <bayyaramharsha@gmail.com>
2023-07-25 11:33:09 +0530

    updated college.sql

--- college.sql
+++ college.sql
@@ -50,3 +50,39 @@ update emp set eno =3  where dno =3;
 update emp set ename = "aakash" where eno=2;
 
 update emp set ename = "mahesh" where eno =3;
+
+
+
+delete from emp where eno=3;
+
+
+insert into emp values(3,"mahesh",50000,3);
+
+insert into emp values(4,"person4",50000,3);
+
+
+insert into emp values(5,"person5",50000,1);
+
+
+update emp set ename = "person4" where eno=4;
+
+insert into emp values(6,"person6",50000,2);
+
+
+
+insert into emp values
+(7,"person7",50000,1),
+(8,"person8",50000,4),
+(9,"person9",50000,2),
+(10,"person10",50000,1),
+(11,"person11",50000,4),
+(12,"person12",50000,6),
+(13,"person13",50000,3),
+(14,"person14",50000,2);
+
+
+update emp set dno = 4 where eno=2;
+
+insert into dept values(4,"civil");
+
+insert into dept values(5,"CSE");
